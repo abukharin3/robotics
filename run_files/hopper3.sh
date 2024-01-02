2b4a2f66cd7cb0cff6d1c65580bc5dbd0765973c
abukharin3 <abukharin3@gatech.edu>
2024-01-02 10:15:28 -0500

    Update run files

--- run_files/hopper3.sh
+++ run_files/hopper3.sh
@@ -5,9 +5,13 @@
 # python train_heuristic.py --env hopper --time_steps 2000000 --seed 1 --order 0,1,2 --alpha 0.3
 # python train_heuristic.py --env hopper --time_steps 2000000 --seed 2 --order 0,1,2 --alpha 0.3
 
-python train_heron.py --env hopper --time_steps 2000000 --seed 0 --order 2,0,1 --sigma 1.0,1.0,1.0
-python train_heron.py --env hopper --time_steps 2000000 --seed 1 --order 2,0,1 --sigma 1.0,1.0,1.0
-python train_heron.py --env hopper --time_steps 2000000 --seed 2 --order 2,0,1 --sigma 1.0,1.0,1.0
+# python train_heron.py --env hopper --time_steps 2000000 --seed 0 --order 2,0,1 --sigma 1.0,1.0,1.0
+# python train_heron.py --env hopper --time_steps 2000000 --seed 1 --order 2,0,1 --sigma 1.0,1.0,1.0
+# python train_heron.py --env hopper --time_steps 2000000 --seed 2 --order 2,0,1 --sigma 1.0,1.0,1.0
+
+python train_heuristic.py --env hopper --time_steps 2000000 --seed 0 --order 0,2,1 --alpha 0.6
+python train_heuristic.py --env hopper --time_steps 2000000 --seed 1 --order 0,2,1 --alpha 0.6
+python train_heuristic.py --env hopper --time_steps 2000000 --seed 2 --order 0,2,1 --alpha 0.6
 
 
 
